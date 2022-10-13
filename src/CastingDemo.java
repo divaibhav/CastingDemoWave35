a4a882a1c7c18e783b53877ca166f1cf4a3ffe80
Vaibhav Diwan <vaibhav.diwan@niit.com>
2022-10-13 19:39:23 +0530

    Initial commit

---
+++ src/CastingDemo.java
@@ -0,0 +1,11 @@
+public class CastingDemo {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 3;
+        double result =  (10 + 2.55555555566666);
+                //syntax
+                //(target-type)casting-variable or literal
+
+        System.out.println("result = " + result);
+    }
+}
